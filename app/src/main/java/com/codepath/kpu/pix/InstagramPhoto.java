ce2c0de34ebf56b0f925d5c036dd86b20af789ab
Kenneth Pu <kp295@cornell.edu>
2016-02-06 16:18:40 -0800

    initial barebones implementation (just captions and photos)

---
+++ app/src/main/java/com/codepath/kpu/pix/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.codepath.kpu.pix;
+
+/**
+ * Created by kpu on 2/6/16.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
